489fdfa143a0855b7e120530168e480dd1a530b8
mjmandelah07 <aramidemojisolagrace@gmail.com>
2025-10-24 01:35:21 +0100

    update shipping

--- src/components/common/others-accordion/ShippingInfoContent.tsx
+++ src/components/common/others-accordion/ShippingInfoContent.tsx
@@ -4,10 +4,10 @@ const ShippingInfoContent: React.FC = () => (
   <div className="space-y-4 text-sm sm:text-base text-gray-800">
     <div>
       <h3 className="font-semibold mb-1">Order Processing & Delivery</h3>
-      <p>All orders placed on the website are subject to a 20-25 business day (excluding potential delays) processing window PRIOR to shipment (processing time = time used to make your unit). Once shipped, here are the approximate delivery times based on your location:</p>
+      <p>All orders placed on the website are subject to a 10-14 business day (excluding potential delays) processing window PRIOR to shipment (processing time = time used to make your unit). Once shipped, here are the approximate delivery times based on your location:</p>
       <ul className="list-disc pl-5">
         <li>Domestic: 3-5 Business Days</li>
-        <li>International: 5-7 Business Days</li>
+        <li>International: 5-10 Business Days</li>
       </ul>
       <p>When your order is shipped out you will receive a shipping confirmation email containing your tracking information.</p>
     </div>
@@ -25,9 +25,9 @@ const ShippingInfoContent: React.FC = () => (
     <div>
       <h4 className="font-semibold">Returns & Exchanges</h4>
       <p>We only accept returns if the wig is damaged by us.</p>
-      <ul className="list-decimal pl-5">
-        <li><b>Return shipping costs:</b> Customers are responsible for return postage costs. We will issue a return label via email & the cost will be deducted from your refund.</li>
-      </ul>
+      <ol className="list-decimal pl-5">
+        <li><b>Return shipping costs:</b> Customers are responsible for return postage costs.</li>
+      </ol>
     </div>
   </div>
 );
